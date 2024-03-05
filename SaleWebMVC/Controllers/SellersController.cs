04d23ba7461e02016bcc298c1ea55c9d34b6e6a3
Andre Cassarro Salvini <andrecassarros@gmail.com>
2024-03-04 23:08:21 -0300

    Simple Create form

--- SaleWebMVC/Controllers/SellersController.cs
+++ SaleWebMVC/Controllers/SellersController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using SaleWebMVC.Services;
+using SaleWebMVC.Models;
 
 namespace SaleWebMVC.Controllers
 {
@@ -20,6 +21,19 @@ namespace SaleWebMVC.Controllers
 		{
 			var list = _sellerService.FindAll();
 			return View(list);
+		}
+
+		public IActionResult Create()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Create(Seller seller)
+		{
+			_sellerService.Insert(seller);
+			return RedirectToAction(nameof(Index));
 		}
 	}
 }
