c7c73a0092555492b333c965b2bc9b0c58c710c8
Andre Cassarro Salvini <andrecassarros@gmail.com>
2024-03-10 02:31:49 -0300

    Exception handling for delete (referential integrity)

---
+++ SaleWebMVC/Services/Exception/IntegrityException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace SaleWebMVC.Services.Exception
+{
+	public class IntegrityException : ApplicationException
+	{
+		public IntegrityException(string message) : base(message)
+		{
+
+		}
+	}
+}
--- SaleWebMVC/Services/SellerService.cs
+++ SaleWebMVC/Services/SellerService.cs
@@ -36,9 +36,16 @@ namespace SaleWebMVC.Services
 
         public async Task RemoveAsync(int id)
         {
-            var obj = await _context.Seller.FindAsync(id);
-            _context.Seller.Remove(obj); 
-           await _context.SaveChangesAsync();
+            try
+            {
+                var obj = await _context.Seller.FindAsync(id);
+                _context.Seller.Remove(obj);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException e)
+			{
+                throw new IntegrityException("O vendedor não pode ser deletado pois existe uma venda vinculada a ele!");
+			}
         }
 
         public async Task UpdateAsync(Seller obj)
