30b8dca6d2ea0d653fbb63c49994d666847d55fd
Andre Cassarro Salvini <andrecassarros@gmail.com>
2024-03-10 02:15:41 -0300

    Asynchronous operations using Tasks (async, await

--- SaleWebMVC/Services/DepartmentService.cs
+++ SaleWebMVC/Services/DepartmentService.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace SaleWebMVC.Services
 {
@@ -16,9 +17,10 @@ namespace SaleWebMVC.Services
 			_context = context;
 		}
 
-		public List<Department> FindAll()
+		//Essa configuração serve para informar ao compilador que essa será assíncrona 
+		public async Task<List<Department>> FindAllAsync()
 		{
-			return _context.Department.OrderBy(x => x.Name).ToList();
+			return await _context.Department.OrderBy(x => x.Name).ToListAsync();
 
 		}
 	}
