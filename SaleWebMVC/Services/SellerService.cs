350688b2df1464ba38012912d23168d94b7c784f
Andre Cassarro Salvini <andrecassarros@gmail.com>
2024-03-09 22:27:27 -0300

    Seller details and eager loading

--- SaleWebMVC/Services/SellerService.cs
+++ SaleWebMVC/Services/SellerService.cs
@@ -1,42 +1,43 @@
-﻿using System;
+﻿using SaleWebMVC.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using SaleWebMVC.Data;
-using SaleWebMVC.Models;
 
 namespace SaleWebMVC.Services
 {
-	public class SellerService
-	{
-		private readonly SaleWebMVCContext _context;
+    public class SellerService
+    {
+        private readonly SaleWebMVCContext _context;
 
-		public SellerService(SaleWebMVCContext context)
-		{
-			_context = context;
-		}
+        public SellerService(SaleWebMVCContext context)
+        {
+            _context = context;
+        }
 
-		public List<Seller> FindAll()
-		{
-			return _context.Seller.ToList();
-		}
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
 
-		public void Insert(Seller obj)
-		{
-			_context.Add(obj);
-			_context.SaveChanges();
-		}
+        public void Insert(Seller obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
 
-		public Seller FindById(int id)
-		{
-			return _context.Seller.FirstOrDefault(obj => obj.Id == id);
-		}
+        public Seller FindById(int id)
+        {
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
+        }
 
-		public void Remove(int id)
-		{
-			var obj = _context.Seller.Find(id);
-			_context.Seller.Remove(obj);
-			_context.SaveChanges();
-		}
-	}
+        public void Remove(int id)
+        {
+            var obj = _context.Seller.Find(id);
+            _context.Seller.Remove(obj);
+            _context.SaveChanges();
+        }
+    }
 }
