d8954fcde931d7e8fd3fe891efccf4d8c0e3915a
Andre Cassarro Salvini <andrecassarros@gmail.com>
2024-03-10 23:41:33 -0300

    Implementing grouping search

--- SaleWebMVC/Models/SalesRecord.cs
+++ SaleWebMVC/Models/SalesRecord.cs
@@ -10,6 +10,7 @@ namespace SaleWebMVC.Models
 		public int Id { get; set; }
 		[DisplayFormat(DataFormatString = "{0:00 dd/MM/yyyy}")]
 		public DateTime Date { get; set; }
+		[DisplayFormat(DataFormatString = "{0:F2")]
 		public double Amount { get; set; }
 		public SaleStatus Status { get; set; }
 		public Seller Seller { get; set; }
